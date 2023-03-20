6eac4aa67db810123f74acdda7be6c0d837e2b2d
bael4 <bayel.ryspekov@gmail.com>
2023-03-20 14:30:16 +0600

    add label

--- GitHub_Demo/ViewController.swift
+++ GitHub_Demo/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+let label = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        view.addSubview(label)
     }
 
 
